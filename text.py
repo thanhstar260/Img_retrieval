510dc7ac8f95268ad723241e76a5781553c604d1
thanhstar <thanhstar260@gmail.com>
2023-11-10 09:46:36 +0700

    integrated func into code

--- text.py
+++ text.py
@@ -30,6 +30,53 @@ with open(image_path_dict, "r") as json_file:
 print(len(image_path))
 
 
+
+def text2img(model,text_query,k,device):
+
+    text_tokens = clip.tokenize([text_query]).to(device)# (1,77)
+
+    with torch.no_grad():
+        text_features = model.encode_text(text_tokens).float() #(1,512)
+
+
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    print("clip_feature: ",clip_feature.shape) # SHAPE(246,512)
+    print("image_feature: ",text_features.shape) 
+
+
+    # CALCULATE DISTANCE
+    distance = np.linalg.norm(clip_feature - text_features.cpu().numpy(),axis = 1 )
+    print(distance.shape)
+
+
+    # SHOW RESULT
+
+    ids = np.argsort(distance)[:k]
+    # print(ids)
+
+    result = [(image_path[str(id)],distance[id]) for id in ids]
+
+    return result
+
+def visualize(result,k):
+    axes = []
+    grid_size = k//8
+    fig = plt.figure(figsize=(10,5))
+
+    for id in range(k):
+        draw_image = result[id]
+        axes.append(fig.add_subplot(grid_size + 1, 8, id+1))
+        axes[-1].set_title(draw_image[0][-17:-4])
+        axes[-1].set_xticks([])
+        axes[-1].set_yticks([])
+        plt.imshow(Image.open(draw_image[0]))
+
+
+    fig.tight_layout()
+    plt.show()
+
+
+
 # LOAD MODEL
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -39,43 +86,7 @@ model, preprocess = clip.load("ViT-B/32")
 model.to(device).eval()
 
 text = "a dolphin and pink ball" 
-# Keyframe = "L01_V001/0147.jpg"
-text_tokens = clip.tokenize([text]).to(device)# (1,77)
+K = 40
 
-with torch.no_grad():
-    text_features = model.encode_text(text_tokens).float() #(1,512)
-
-
-text_features /= text_features.norm(dim=-1, keepdim=True)
-print("clip_feature: ",clip_feature.shape) # SHAPE(246,512)
-print("image_feature: ",text_features.shape) 
-
-
-# CALCULATE DISTANCE
-distance = np.linalg.norm(clip_feature - text_features.cpu().numpy(),axis = 1 )
-print(distance.shape)
-
-
-# SHOW RESULT
-k = 40
-ids = np.argsort(distance)[:k]
-# print(ids)
-
-result = [(image_path[str(id)],distance[id]) for id in ids]
-
-
-axes = []
-grid_size = k//8
-fig = plt.figure(figsize=(10,5))
-
-for id in range(k):
-    draw_image = result[id]
-    axes.append(fig.add_subplot(grid_size + 1, 8, id+1))
-    axes[-1].set_title(draw_image[0][-17:-4])
-    axes[-1].set_xticks([])
-    axes[-1].set_yticks([])
-    plt.imshow(Image.open(draw_image[0]))
-
-
-fig.tight_layout()
-plt.show()
+result = text2img(model, text, K, device)
+visualize(result, K)
