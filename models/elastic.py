9ac6ad10f0e65c1ff2d844e3a0a745a202a2de83
thanhstar <thanhstar260@gmail.com>
2024-07-19 09:16:44 +0700

    combine into 1 class

--- models/elastic.py
+++ models/elastic.py
@@ -1,7 +1,7 @@
 import json
 from elasticsearch import Elasticsearch, helpers
 import os
-
+import numpy as np
 import sys
 sys.path.append(r"./")
 from models.utils import visualize, load_image_path
@@ -18,7 +18,7 @@ class Elastic():
             Elasticsearch: Đối tượng Elasticsearch.
         """
         
-        print(check_server)
+        # print(check_server)
         if check_server == "server":
             self.es = Elasticsearch([{'host': host, 'port': port, 'scheme': scheme}])
         else:
@@ -32,7 +32,7 @@ class Elastic():
         else:
             print(f"Chỉ mục {index_name} đã tồn tại.")
             
-    def upload_data_to_elastic(self, data_path, index_name="objects", id_name=None):
+    def upload_data_to_elastic(self, data_path, index_name= None, id_name=None):
         """
         Đọc file JSON và thêm các phần tử vào Elasticsearch sau khi map class_name.
         
@@ -70,52 +70,7 @@ class Elastic():
         # Sử dụng helpers.bulk để thêm dữ liệu vào Elasticsearch
         helpers.bulk(self.es, actions)
         
-        
-    def delete_index(self, index_name):
-        # Kiểm tra xem index có tồn tại không
-        if self.es.indices.exists(index=index_name):
-            # Xóa index
-            self.es.indices.delete(index=index_name)
-            print(f"Index '{index_name}' đã được xóa.")
-        else:
-            print(f"Index '{index_name}' không tồn tại.")
-            
-            
-
-class OCR(Elastic):
-    def __init__(self):
-        super().__init__()
-        
-    def upload_data_to_elastic(self, data_path, index_name="objects"):
-            """
-            Đọc file JSON và thêm các phần tử vào Elasticsearch sau khi map class_name.
-            
-            Args:
-                es (Elasticsearch): Đối tượng Elasticsearch.
-                data_path (str): Đường dẫn tới file JSON chứa dữ liệu.
-                class_name_path (str): Đường dẫn tới file class_name.
-                index_name (str): Tên index trong Elasticsearch.
-            """
-            
-            # Đọc toàn bộ nội dung của file JSON chứa dữ liệu
-            with open(data_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-            
-            
-            # Tạo danh sách các hành động để thêm vào Elasticsearch
-            actions = [
-                {
-                    "_index": index_name,
-                    # "_id": item["index"],  # Sử dụng "index" làm "_id"
-                    "_source": item
-                }
-                for item in data
-            ]
-            
-            # Sử dụng helpers.bulk để thêm dữ liệu vào Elasticsearch
-            helpers.bulk(self.es, actions)
-            
-    def ocr_retrieval(self, text_query, k, index_name="ocr"):
+    def Elastic_retrieval(self, text_query, k, index_name="ocr"):
 
         query = {
             "query": {
@@ -149,79 +104,164 @@ class OCR(Elastic):
         scores = [hit['_score'] for hit in res['hits']['hits']]
         # print(ids)
         # print(scores)
-        return ids        
-    
-    
-class ASR(Elastic):
-    def __init__(self):
-        super().__init__()
+        scores = np.array(scores)
+        scr_result = 1 / (1 + np.exp(-scores))
+        scr_result = scr_result.tolist()
         
-    def upload_data_to_elastic(self, data_path, index_name="objects"):
-            """
-            Đọc file JSON và thêm các phần tử vào Elasticsearch sau khi map class_name.
-            
-            Args:
-                es (Elasticsearch): Đối tượng Elasticsearch.
-                data_path (str): Đường dẫn tới file JSON chứa dữ liệu.
-                class_name_path (str): Đường dẫn tới file class_name.
-                index_name (str): Tên index trong Elasticsearch.
-            """
-            
-            # Đọc toàn bộ nội dung của file JSON chứa dữ liệu
-            with open(data_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
+        return ids, scr_result
+    
+    
+    def delete_index(self, index_name):
+        # Kiểm tra xem index có tồn tại không
+        if self.es.indices.exists(index=index_name):
+            # Xóa index
+            self.es.indices.delete(index=index_name)
+            print(f"Index '{index_name}' đã được xóa.")
+        else:
+            print(f"Index '{index_name}' không tồn tại.")
             
             
-            # Tạo danh sách các hành động để thêm vào Elasticsearch
-            actions = [
-                {
-                    "_index": index_name,
-                    # "_id": item["index"],  # Sử dụng "index" làm "_id"
-                    "_source": item
-                }
-                for item in data
-            ]
-            
-            # Sử dụng helpers.bulk để thêm dữ liệu vào Elasticsearch
-            helpers.bulk(self.es, actions)
-            
-    def asr_retrieval(self, text_query, k, index_name="asr"):
-        query = {
-            "query": {
-                "bool": {
-                    "should": [
-                        {
-                            "multi_match": {
-                                "query": text_query,
-                                "fields": ["text"],
-                                "type": "phrase",
-                                "boost": 2  # Tăng cường độ ưu tiên cho kết quả chính xác
-                            }
-                        },
-                        {
-                            "multi_match": {
-                                "query": text_query,
-                                "fields": ["text"],
-                                "fuzziness": "AUTO"
-                            }
-                        }
-                    ]
-                }
-            },
-            "size": k
-        }
 
-        # Thực hiện tìm kiếm
-        res = self.es.search(index=index_name, body=query)
-        # print(res)
-        start = [hit['_source']["start"] for hit in res['hits']['hits']]
-        end = [hit['_source']["end"] for hit in res['hits']['hits']]
-        # scores = [hit['_score'] for hit in res['hits']['hits']]
-        ids = []
-        for i in range(len(start)):
-            id = list(range(int(start[i]), int(end[i])+1))
-            ids.extend(id)
-        return ids[:k]
+# class OCR(Elastic):
+#     def __init__(self):
+#         super().__init__()
+        
+#     def upload_data_to_elastic(self, data_path, index_name="objects"):
+#             """
+#             Đọc file JSON và thêm các phần tử vào Elasticsearch sau khi map class_name.
+            
+#             Args:
+#                 es (Elasticsearch): Đối tượng Elasticsearch.
+#                 data_path (str): Đường dẫn tới file JSON chứa dữ liệu.
+#                 class_name_path (str): Đường dẫn tới file class_name.
+#                 index_name (str): Tên index trong Elasticsearch.
+#             """
+            
+#             # Đọc toàn bộ nội dung của file JSON chứa dữ liệu
+#             with open(data_path, "r", encoding="utf-8") as file:
+#                 data = json.load(file)
+            
+            
+#             # Tạo danh sách các hành động để thêm vào Elasticsearch
+#             actions = [
+#                 {
+#                     "_index": index_name,
+#                     # "_id": item["index"],  # Sử dụng "index" làm "_id"
+#                     "_source": item
+#                 }
+#                 for item in data
+#             ]
+            
+#             # Sử dụng helpers.bulk để thêm dữ liệu vào Elasticsearch
+#             helpers.bulk(self.es, actions)
+            
+#     def ocr_retrieval(self, text_query, k, index_name="ocr"):
+
+#         query = {
+#             "query": {
+#                 "bool": {
+#                     "should": [
+#                         {
+#                             "multi_match": {
+#                                 "query": text_query,
+#                                 "fields": ["text"],
+#                                 "type": "phrase",
+#                                 "boost": 2  # Tăng cường độ ưu tiên cho kết quả chính xác
+#                             }
+#                         },
+#                         {
+#                             "multi_match": {
+#                                 "query": text_query,
+#                                 "fields": ["text"],
+#                                 "fuzziness": "AUTO"
+#                             }
+#                         }
+#                     ]
+#                 }
+#             },
+#             "size": k
+#         }
+
+#         # Thực hiện tìm kiếm
+#         res = self.es.search(index=index_name, body=query)
+#         # print(res)
+#         ids = [hit['_source']["id"] for hit in res['hits']['hits']]
+#         scores = [hit['_score'] for hit in res['hits']['hits']]
+#         # print(ids)
+#         # print(scores)
+#         return ids        
+    
+    
+# class ASR(Elastic):
+#     def __init__(self):
+#         super().__init__()
+        
+#     def upload_data_to_elastic(self, data_path, index_name="objects"):
+#             """
+#             Đọc file JSON và thêm các phần tử vào Elasticsearch sau khi map class_name.
+            
+#             Args:
+#                 es (Elasticsearch): Đối tượng Elasticsearch.
+#                 data_path (str): Đường dẫn tới file JSON chứa dữ liệu.
+#                 class_name_path (str): Đường dẫn tới file class_name.
+#                 index_name (str): Tên index trong Elasticsearch.
+#             """
+            
+#             # Đọc toàn bộ nội dung của file JSON chứa dữ liệu
+#             with open(data_path, "r", encoding="utf-8") as file:
+#                 data = json.load(file)
+            
+            
+#             # Tạo danh sách các hành động để thêm vào Elasticsearch
+#             actions = [
+#                 {
+#                     "_index": index_name,
+#                     # "_id": item["index"],  # Sử dụng "index" làm "_id"
+#                     "_source": item
+#                 }
+#                 for item in data
+#             ]
+            
+#             # Sử dụng helpers.bulk để thêm dữ liệu vào Elasticsearch
+#             helpers.bulk(self.es, actions)
+            
+#     def asr_retrieval(self, text_query, k, index_name="asr"):
+#         query = {
+#             "query": {
+#                 "bool": {
+#                     "should": [
+#                         {
+#                             "multi_match": {
+#                                 "query": text_query,
+#                                 "fields": ["text"],
+#                                 "type": "phrase",
+#                                 "boost": 2  # Tăng cường độ ưu tiên cho kết quả chính xác
+#                             }
+#                         },
+#                         {
+#                             "multi_match": {
+#                                 "query": text_query,
+#                                 "fields": ["text"],
+#                                 "fuzziness": "AUTO"
+#                             }
+#                         }
+#                     ]
+#                 }
+#             },
+#             "size": k
+#         }
+
+#         # Thực hiện tìm kiếm
+#         res = self.es.search(index=index_name, body=query)
+#         # print(res)
+#         start = [hit['_source']["start"] for hit in res['hits']['hits']]
+#         end = [hit['_source']["end"] for hit in res['hits']['hits']]
+#         # scores = [hit['_score'] for hit in res['hits']['hits']]
+#         ids = []
+#         for i in range(len(start)):
+#             id = list(range(int(start[i]), int(end[i])+1))
+#             ids.extend(id)
+#         return ids[:k]
     
     
     
